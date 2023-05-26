96561375853a8196d819d900bb0ea373441d925f
Hunter <98065056+TrashDeviance@users.noreply.github.com>
2023-05-25 22:33:20 -0500

    FIxed issue with user data not going to DB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,14 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 
 // export const environment = {
 //   production: false,
 //   firebase: {
 //     projectId: 'lccc-951e9',
 //     appId: '1:490603146752:web:9667961e622f67770172e5',
-//     databaseURL: 'https://lccc-951e9-default-rtdb.firebaseio.com/',
+//     databaseURL: 'https://lccc-951e9-default-rtdb.firebaseio.com',
 //     storageBucket: 'lccc-951e9.appspot.com',
 //     apiKey: 'AIzaSyB7ZIv9yI955npmzc5MSarDB4OJ75aM5p8',
 //     authDomain: 'lccc-951e9.firebaseapp.com',
@@ -21,7 +22,7 @@ export const environment = {
   firebase: {
     projectId: 'lccc-951e9',
     appId: '1:490603146752:web:9667961e622f67770172e5',
-    databaseURL: 'https://lccc-951e9-default-rtdb.firebaseio.com/',
+    databaseURL: 'https://lccc-951e9-default-rtdb.firebaseio.com',
     storageBucket: 'lccc-951e9.appspot.com',
     apiKey: 'AIzaSyB7ZIv9yI955npmzc5MSarDB4OJ75aM5p8',
     authDomain: 'lccc-951e9.firebaseapp.com',
@@ -33,3 +34,5 @@ export const environment = {
 // Initialize Firebase
 export const app = initializeApp(environment.firebase);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
